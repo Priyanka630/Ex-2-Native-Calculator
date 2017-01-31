6a37adc197e32a88481326dfb095135170153214
ITLAB <ITLAB@IT>
2017-01-31 16:11:13 +0530

    six commit

--- src/com/example/nativecal/MainActivity.java
+++ src/com/example/nativecal/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends ActionBarActivity {
 	 
     boolean mAdd , mSub ,mMul ,mDiv ;
  
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
